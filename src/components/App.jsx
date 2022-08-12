5d4bc70c15b37bb97e3bf8aaff37a4995df2de43
Volodymyr Tabashniuk <vtabashniuk@outlook.com>
2022-08-12 23:11:17 +0300

    add task 1.1
    Create component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
+import { Profile } from './Profile/Profile';
+import userInfo from 'data/user.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile {...userInfo} />
+    </>
   );
 };
